244b9f5b0caf9a0885e509be9a4517fead769ed8
MACBOOK PRO RETINA <macbookproretina@MacBook-Pro-de-MACBOOK-2.local>
2018-12-01 18:42:42 +0100

    first commit

---
+++ ViewPager/Controllers/SwipeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SwipeViewController.swift
+//  ViewPager
+//
+//  Created by MACBOOK PRO RETINA on 30/11/2018.
+//  Copyright © 2018 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import Foundation
---
+++ ViewPager/Models/Page.swift
@@ -0,0 +1,9 @@
+//
+//  Page.swift
+//  ViewPager
+//
+//  Created by MACBOOK PRO RETINA on 01/12/2018.
+//  Copyright © 2018 MACBOOK PRO RETINA. All rights reserved.
+//
+
+import Foundation
